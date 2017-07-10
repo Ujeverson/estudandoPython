23a8a69708371d94faa7857f335d7d3e58b4aba8
Ujeverson <ujeverson@gmail.com>
2017-07-10 00:28:17 -0300

    -

---
+++ src/SucessorAntecessor.py
@@ -0,0 +1,6 @@
+print("Vamos saber o que é sucessor e antecessor, ok?")
+numero = int(input("Para isso digite um número inteiro. "))
+print("--" *80)
+sucessor = numero + 1
+antecessor = numero -1
+print("O sucessor de {} é {} e o antecessor é {}" .format(numero, sucessor, antecessor))
